58cc34e4f8a2f976031e3010aa9280ae9175cb23
lucasnahuelcarrizo11-nc <lucasnahuelcarrizo11@gmail.com>
2026-01-09 14:56:28 -0300

    use single axios instance with env baseURL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,13 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "https://adrentar-backend.onrender.com",
 });
-
-console.log("API BASE URL =>", import.meta.env.VITE_API_URL);
 
 export default api;
--- src/service/PagoService.jsx
+++ src/service/PagoService.jsx
@@ -1,7 +1,25 @@
-import axios from "axios";
+import api from "../api/axiosConfig";
 
-const api = axios.create({
-  baseURL: "https://adrentar-backend.onrender.com",
-});
+const BASE_URL = import.meta.env.VITE_API_URL;
 
-export default api;
+const CLIENTE_BASE_REST_API_URL = `${BASE_URL}/api/pagos`;
+
+const crearPago = async (idAlquiler, mes, anio) => {
+  const response = await axios.post(
+    `${CLIENTE_BASE_REST_API_URL}/preference`,
+    null,
+    {
+      params: {
+        idAlquiler,
+        mes,
+        anio
+      }
+    }
+  );
+
+  return response.data;
+};
+
+export default {
+  crearPago
+};
